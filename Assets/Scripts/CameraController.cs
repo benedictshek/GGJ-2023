b814e5db88b28124d026ef59d0681b347ab786a0
benedictshek <benedictshekpersonal@gmail.com>
2023-02-03 01:21:45 +0800

    Updated camera pos when night vision is on

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
@@ -6,13 +7,43 @@ public class CameraController : MonoBehaviour
     [SerializeField] private PostProcessVolume postProcessVolume;
 
     private bool isUsingCamera;
+
+    private Vector3 cameraInitPos;
+    [SerializeField] private Transform camNightVisionOnTransform;
+
+    private Sway camSway;
     
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        camSway = GetComponent<Sway>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        cameraInitPos = transform.localPosition;
+    }
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(1))
         {
             if(!isUsingCamera) return;
             postProcessVolume.enabled = !postProcessVolume.enabled;
+
+            if (postProcessVolume.enabled)
+            {
+                audioSource.Play();
+                camSway.enabled = false;
+                transform.DOLocalMove(camNightVisionOnTransform.localPosition, 0.5f);
+            }
+            else
+            {
+                camSway.enabled = true;
+                transform.DOLocalMove(cameraInitPos, 0.5f);
+            }
         }
     }
     
