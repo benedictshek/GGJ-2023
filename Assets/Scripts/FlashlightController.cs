71d5ee118c2bfa0b3d6da34e9fab106f8c792ba0
benedictshek <benedictshekpersonal@gmail.com>
2023-02-02 22:42:49 +0800

    Added switch item animation

--- Assets/Scripts/FlashlightController.cs
+++ Assets/Scripts/FlashlightController.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class FlashlightController : MonoBehaviour
 {
-    [SerializeField] private new Light light;
+    [SerializeField] private Light light;
     private AudioSource audioSource;
+
+    public bool isUsingFlashLight = true;
 
     private void Awake()
     {
@@ -14,8 +16,20 @@ public class FlashlightController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(1))
         {
+            if(!isUsingFlashLight) return;
             audioSource.Play();
             light.enabled = !light.enabled;
         }
+    }
+
+    public void EnableFlashLight()
+    {
+        isUsingFlashLight = true;
+    }
+    
+    public void DisableFlashLight()
+    {
+        isUsingFlashLight = false;
+        light.enabled = false;
     }
 }
--- Assets/Scripts/SwitchHandItems.cs
+++ Assets/Scripts/SwitchHandItems.cs
@@ -1,3 +1,5 @@
+using System;
+using DG.Tweening;
 using UnityEngine;
 
 public class SwitchHandItems : MonoBehaviour
@@ -5,23 +7,54 @@ public class SwitchHandItems : MonoBehaviour
     [SerializeField] private GameObject flashLight;
     [SerializeField] private GameObject camera;
 
+    [SerializeField] private Transform itemHideTransform;
+
+    private Vector3 flashLightShowPos;
+    private Vector3 cameraShowPos;
+
+    [SerializeField] private float showItemDuration;
+
+    private FlashlightController flashlightController;
+
+    private void Awake()
+    {
+        flashlightController = flashLight.GetComponent<FlashlightController>();
+    }
+
     private void Start()
     {
-        flashLight.SetActive(false);
-        camera.SetActive(false);
+        //flashLight.SetActive(false);
+        //camera.SetActive(false);
+
+        flashLightShowPos = flashLight.transform.localPosition;
+        cameraShowPos = camera.transform.localPosition;
+
+        camera.transform.DOLocalMove(itemHideTransform.localPosition, showItemDuration);
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            flashLight.SetActive(true);
-            camera.SetActive(false);
+            //flashLight.SetActive(true);
+            //camera.SetActive(false);
+            
+            var itemHideLocalPosition = itemHideTransform.localPosition;
+            flashLight.transform.DOLocalMove(flashLightShowPos, showItemDuration).From(itemHideLocalPosition);
+            camera.transform.DOLocalMove(itemHideLocalPosition, showItemDuration).From(cameraShowPos);
+            
+            flashlightController.EnableFlashLight();
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            flashLight.SetActive(false);
-            camera.SetActive(true);
+            //flashLight.SetActive(false);
+            //camera.SetActive(true);
+
+            var itemHideLocalPosition = itemHideTransform.localPosition;
+            flashLight.transform.DOLocalMove(itemHideLocalPosition, showItemDuration).From(flashLightShowPos);
+            camera.transform.DOLocalMove(cameraShowPos, showItemDuration).From(itemHideLocalPosition);
+            
+            flashlightController.DisableFlashLight();
         }
     }
 }
